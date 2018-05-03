5e03c1926e5babd863fe5d504a1bbf00447b8f2f
Maria  Morris <mariasemailbox@gmail.com>
2018-05-03 01:47:11 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def turn_count(board)
+  turn = 0
+  board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1
+    end
+end
+return turn
+end
+
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
+end
